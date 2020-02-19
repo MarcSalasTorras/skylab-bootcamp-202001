36ff8afd5b4c20ad81ce013976d4d06abc8f0720
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-02-19 19:00:54 +0100

    end express server, starting the new chalence

--- staff/marc-salas/web-server/index.js
+++ staff/marc-salas/web-server/index.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const path = require('path')
-const logger = require('./logger')
+const logger = require('./utils/logger')
+const loggerMidWare = require('./utils/logger-mid-ware')
+
+logger.level = logger.DEBUG
+logger.path = path.join('./server.log')
+
+logger.debug('setting up server')
 
 const app = express()
 
 const { argv: [, , port = 8080] } = process
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(loggerMidWare)
 
-app.use((res, req, next) => {
-    
-})
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => logger.info(`server up and running on port ${port}`))
 
-function errorHandler (err, req, res, next) {
-    if (res.headersSent) {
-      return next(err)
-    }
-    res.status(500)
-    res.render('error', { error: err })
-  }
 process.on('SIGINT', () => {
-    logger.warn(`server abruptly stopped`)
-
-    setTimeout(() => process.exit(0), 1000)
+  logger.warn(`server abruptly stopped`)
+  process.exit(0)
 })
