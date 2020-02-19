682ebe695174957ea9a31135108edfe5608cd843
MarcSalasTorras <marc.salas.torras@gmail.com>
2020-02-19 17:53:17 +0100

    update

--- staff/marc-salas/web-server/index.js
+++ staff/marc-salas/web-server/index.js
@@ -8,8 +8,19 @@ const { argv: [, , port = 8080] } = process
 
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use((res, req, next) => {
+    
+})
+
 app.listen(port, () => logger.info(`server up and running on port ${port}`))
 
+function errorHandler (err, req, res, next) {
+    if (res.headersSent) {
+      return next(err)
+    }
+    res.status(500)
+    res.render('error', { error: err })
+  }
 process.on('SIGINT', () => {
     logger.warn(`server abruptly stopped`)
 
